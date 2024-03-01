6874a7e2d601061ccdddf53ed39bf41c6a76a349
sumann7916 <khadkasuman324@gmail.com>
2024-03-02 00:35:32 +0545

    feat(hero): design hero page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), mdx(), image()],
+	integrations: [
+		tailwind(),
+		mdx(),
+		image(),
+		react({
+			include: ['**/react/*s'],
+		}),
+	],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,34 @@
 const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
+const { default: flattenColorPalette } = require('tailwindcss/lib/util/flattenColorPalette');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
+		extend: {
+			animation: {
+				'meteor-effect': 'meteor 5s linear infinite',
+			},
+			scroll:
+				'scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite',
+			keyframes: {
+				scroll: {
+					to: {
+						transform: 'translate(calc(-50% - 0.5rem))',
+					},
+				},
+				meteor: {
+					'0%': { transform: 'rotate(215deg) translateX(0)', opacity: '1' },
+					'70%': { opacity: '1' },
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: '0',
+					},
+				},
+			},
+		},
 		screens: {
 			xs: '480px',
 			sm: '640px',
@@ -40,6 +64,7 @@ module.exports = {
 		},
 	},
 	plugins: [
+		addVariablesForColors,
 		plugin(function ({ addBase, theme }) {
 			addBase({
 				h2: {
@@ -53,3 +78,14 @@ module.exports = {
 		}),
 	],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme('colors'));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		':root': newVars,
+	});
+}
